dfa28cb34abb1f0bb4e5fd8c4bdafdca5d9ac980
anipimpale94 <pimpale.anirudha1994@gmail.com>
2023-03-10 02:09:56 +0530

    Removing comments

--- 2023/python/largest-rectangle-histogram.py
+++ 2023/python/largest-rectangle-histogram.py
@@ -1,16 +1,5 @@
 # Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
 # 84. Largest Rectangle in Histogram
-# Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, 
-# return the area of the largest rectangle in the histogram.
-#
-# Example
-# Input: heights = [2,1,5,6,2,3]
-# Output: 10
-# Explanation: The above is a histogram where width of each bar is 1.
-# The largest rectangle is shown in the red area, which has an area = 10 units.
-#
-# Input: heights = [2,4]
-# Output: 4
 
 from typing import List
 
--- 2023/python/minimum-window-substring.py
+++ 2023/python/minimum-window-substring.py
@@ -1,21 +1,5 @@
 # https://leetcode.com/problems/minimum-window-substring/
 # 76. Minimum Window Substring
-# Given two strings s and t of lengths m and n respectively, return the minimum window 
-# substring of s such that every character in t (including duplicates) is included in the window. 
-# If there is no such substring, return the empty string "".
-#
-# Example
-# Input: s = "ADOBECODEBANC", t = "ABC"
-# Output: "BANC"
-# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
-#
-# Input: s = "a", t = "a"
-# Output: "a"
-# Explanation: The entire string s is the minimum window.
-# Input: s = "a", t = "aa"
-# Output: ""
-# Explanation: Both 'a's from t must be included in the window.
-# Since the largest window of s only has one 'a', return empty string.
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
